43f16965b240a1ee9ca7c5d6005e426c85e190e9
yusei-wy <yusei.kasa@gmail.com>
2020-09-13 15:33:22 +0900

    fix: .config は全てのファイルを dotfiles で管理しないとシンボリックリンクの作成に失敗していた

--- init.sh
+++ init.sh
@@ -2,10 +2,7 @@
 
 source _utils.sh
 
-DOT_FILES=(.bashrc .bash_aliases .profile .gitconfig .gitignore_global)
-
-# make dirs
-mkdir -p $HOME/.config/nvim
+DOT_FILES=(.bashrc .bash_aliases .profile .gitconfig .gitignore_global .config)
 
 for file in ${DOT_FILES[@]}
 do
@@ -14,9 +11,6 @@ do
     fi
 done
 
-if ! [ -d $HOME/.config/nvim ]; then
-    ln -s $HOME/dotfiles/.config/nvim $HOME/.config
-fi
 if ! [ -f $HOME/init.vim ]; then
     ln -s $HOME/dotfiles/.config/nvim/init.vim $HOME/init.vim
 fi
