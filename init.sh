0982fa18fca10361b45ea49cd8685306b029c2f6
yusei-wy <yusei.kasa@gmail.com>
2020-09-13 15:09:38 +0900

    add: vim 関連のシンボリックリンクを追加

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_aliases .profile .gitconfig .gitignore_global .config/nvim/init.vim)
+source _utils.sh
+
+DOT_FILES=(.bashrc .bash_aliases .profile .gitconfig .gitignore_global)
 
 # make dirs
 mkdir -p $HOME/.config/nvim
@@ -11,3 +13,16 @@ do
         ln -s $HOME/dotfiles/$file $HOME/$file
     fi
 done
+
+if ! [ -d $HOME/.config/nvim ]; then
+    ln -s $HOME/dotfiles/.config/nvim $HOME/.config
+fi
+if ! [ -f $HOME/init.vim ]; then
+    ln -s $HOME/dotfiles/.config/nvim/init.vim $HOME/init.vim
+fi
+if ! [ -f $HOME/lazy.toml ]; then
+    ln -s $HOME/dotfiles/.config/nvim/dein/lazy.toml $HOME/lazy.toml
+fi
+if ! [ -f $HOME/plugins.toml ]; then
+    ln -s $HOME/dotfiles/.config/nvim/dein/plugins.toml $HOME/plugins.toml
+fi
